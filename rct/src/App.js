b7d4661e7b81f03919e5a1e95f73f56376b81665
R <subash.zinavo@gmail.com>
2019-11-06 10:12:30 +0530

    new file added

--- rct/src/App.js
+++ rct/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import sampleRc from './sampleRc.js';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <h1>{sampleRc}</h1>
       </header>
     </div>
   );
---
+++ rct/src/sampleRc.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function sampleRc(){
+return (
+   
+  <section> new react</section>  
+);
+}
+export default App;
